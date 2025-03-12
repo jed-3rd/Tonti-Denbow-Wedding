4a1ecac72ff0874357d67bae9d19771459f2cfc2
Jed <116396483+jed-3rd@users.noreply.github.com>
2025-03-11 20:11:44 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -47,6 +47,7 @@ export default defineConfig({
 			SVG: false, // astro-icon handles this
 		}),
 	],
+	config: pnpm(),
 	vite: {
 		plugins: [tailwindcss()],
 		// stop inlining short scripts to fix issues with ClientRouter: https://github.com/withastro/astro/issues/12804
